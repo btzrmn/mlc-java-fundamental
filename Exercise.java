92290a7c5041a3aaf7d2ef306e245738da1867ab
batzorig <batzorig.b@nomadicss.mn>
2024-04-27 12:12:56 +0800

    updated

--- Exercise.java
+++ Exercise.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Exercise {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
     }
 }
